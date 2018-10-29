42b5de54cb5bbd6e874add04b00e09ebc4f27081
geromwatson <42458359+geromwatson@users.noreply.github.com>
2018-10-29 00:33:45 -0600

    Docu Interna

--- src/Estructuras/Dragon.java
+++ src/Estructuras/Dragon.java
@@ -1,31 +1,45 @@
 
 package Estructuras;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *Clase Dragon donde se definen los atributos de cada Dragon para su creacion y manipulacion
+ * @author All
+ */
+@XmlRootElement(name = "dragon")
+@XmlType(propOrder = {"nombre","velocidadRecarga", "edad", "resistencia","clase","padre", "pos"})
+ 
 
 public class Dragon {
-    
+    /**Atributos que tienen todos los Dragones*/
     public String nombre;
     public int velocidadRecarga;
     public int edad;
     public int resistencia;
     public int clase;
     public String padre;
+    public Position pos;
+    /**Atributo Next para estructura de las listas*/ 
     public Dragon next;
+    /**Atributos para crear el Arbol binario por familias*/
     public Dragon father;
     public Dragon leftson;
     public Dragon rightSon;
-    
-    public Position pos;
+    /**Atributos para crear el Arbol AVL por edad*/
+    int numMat;
+    Dragon izqda, drcha;
 
-    public Dragon getNext() {
-        return next;
-    }
-
-    public void setNext(Dragon next) {
-        this.next = next;
-    }
-    
-    
+    /**Constructor clase Dragon donde inicializa los atributos de Dr
+     * @param nombre
+     * @param velocidad
+     * @param edad
+     * @param resistencia
+     * @param clase
+     * @param padre
+     * @param pos*/
     public Dragon(String nombre, int velocidad, int edad, int resistencia, int clase, String padre, Position pos){
         this.nombre = nombre;
         this.velocidadRecarga = velocidad;
@@ -36,93 +50,67 @@ public class Dragon {
         this.pos = pos;
         
     }
-    
-
+    /**Getters y Setter
+     * @return s*/
+    public Dragon getNext() {
+        return next;
+    }
+    public void setNext(Dragon next) {
+        this.next = next;
+    }
+    @XmlAttribute(name = "nombre")
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    @XmlAttribute(name = "velocidadRecarga")
     public int getVelocidadRecarga() {
         return velocidadRecarga;
     }
-
     public void setVelocidadRecarga(int velocidadRecarga) {
         this.velocidadRecarga = velocidadRecarga;
     }
-
+    @XmlAttribute(name = "edad")
     public int getEdad() {
         return edad;
     }
-
     public void setEdad(int edad) {
         this.edad = edad;
     }
-
+    @XmlAttribute(name = "resistencia")
     public int getResistencia() {
         return resistencia;
     }
-
     public void setResistencia(int Resistencia) {
         this.resistencia = Resistencia;
     }
-
+    @XmlAttribute(name = "clase")
     public int getClase() {
         return clase;
     }
-
     public void setClase(int clase) {
         this.clase = clase;
     }
-
+    @XmlAttribute(name = "padre")
     public String getPadre() {
         return padre;
     }
-
     public void setPadre(String padre) {
         this.padre = padre;
     }
-
-    public Dragon getFather() {
-        return father;
-    }
-
-    public void setFather(Dragon father) {
-        this.father = father;
-    }
-
-    public Dragon getLeftson() {
-        return leftson;
-    }
-
-    public void setLeftson(Dragon leftson) {
-        this.leftson = leftson;
-    }
-
-    public Dragon getRightSon() {
-        return rightSon;
-    }
-
-    public void setRightSon(Dragon rightSon) {
-        this.rightSon = rightSon;
-    }
-
+    @XmlAttribute(name = "pos")
     public Position getPos() {
         return pos;
     }
-
     public void setPos(Position pos) {
         this.pos = pos;
     }
     public void setPos(int x, int y){
         this.pos = new Position(x,y);
-    }
+    } 
 
-
-    
-    
     
 }
+
