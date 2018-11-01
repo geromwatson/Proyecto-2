ab0043669aac5b80175775dd182eb170f21e15c7
aaronrs02 <43222229+aaronrs02@users.noreply.github.com>
2018-10-31 21:21:33 -0600

    se añade Bola

---
+++ src/Estructuras/bola.java
@@ -0,0 +1,26 @@
+package Estructuras;
+
+public class bola {
+	int bolax,bolay;
+	
+
+	public int getBolax() {
+		return bolax;
+	}
+
+	public void setBolax(int bolax) {
+		this.bolax = bolax;
+	}
+
+	public int getBolay() {
+		return bolay;
+	}
+
+	public void setBolay(int bolay) {
+		this.bolay = bolay;
+	}
+
+	
+	
+
+}
