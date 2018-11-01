4e932031867f58c12a70238ebb3a5acb1fe75345
geromwatson <42458359+geromwatson@users.noreply.github.com>
2018-11-01 09:42:14 -0600

    Graficacion arbol binario por edad

---
+++ src/Estructuras/Arbol.java
@@ -0,0 +1,135 @@
+
+package Estructuras;
+
+
+public class Arbol {
+    
+    
+    Linked_List listadragones;
+    
+    public void graficarArbol(Linked_List listaDragones){{
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX(), 350);
+        }
+        if (listaDragones.getNodeinIndex(1).getNext() != null){
+            listaDragones.getNodeinIndex(1).setPos(listaDragones.getNodeinIndex(1).getPos().getX()+50, 450);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+50, 250);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+100, 500);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+100, 400);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+100, 300);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+100, 200);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 525);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 475);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 425);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 375);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 325);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 275);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 225);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+150, 175);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 538);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 513);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 488);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 463);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 438);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 413);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 388);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 363);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 338);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 313);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 288);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 263);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 238);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 213);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 188);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 163);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 138);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+200, 113);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+250, 546);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+250, 534);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+250, 522);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+250, 510);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+250, 498);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+250, 486);
+        }
+        if (listaDragones.getNodeinIndex(0).getNext() != null){
+            listaDragones.getNodeinIndex(0).setPos(listaDragones.getNodeinIndex(0).getPos().getX()+250, 474);
+        }
+    }
+        
+    }
+    
+}
